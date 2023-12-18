9889629bad7eed1a48ee6413c39593057a489aaa
Julio <julio.pea@unachi.ac.pa>
2023-12-18 00:42:25 -0500

    final commit

---
+++ src/pages/main.js
@@ -0,0 +1,9 @@
+ScrollReveal().reveal('.section-servicios');
+ScrollReveal().reveal('section-servicios', { delay: 500 });
+
+window.sr = ScrollReveal();
+sr.reveal('.section-servicios',{
+duration: 4000,
+origin:'bottom',
+distance: '-200px'
+});
